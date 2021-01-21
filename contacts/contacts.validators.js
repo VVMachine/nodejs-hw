667efd9bf36db3b3f41eef2bdc59312ddbd4500c
VVMachine <vik.atamanenko@gmail.com>
2021-01-21 10:04:24 +0200

    hw3

--- contacts/contacts.validators.js
+++ contacts/contacts.validators.js
@@ -1,10 +1,15 @@
 const Joi = require("joi");
+const {
+  Types: { ObjectId },
+} = require("mongoose");
 
 function validateNewContact(req, res, next) {
   const newContactValidation = Joi.object({
     name: Joi.string().min(1).required(),
     email: Joi.string().email().required(),
     phone: Joi.string().min(10).required(),
+    subscription: Joi.string().required(),
+    password: Joi.string().min(8).required(),
   });
 
   const validationResult = newContactValidation.validate(req.body);
@@ -24,13 +29,27 @@ function validatePatchContact(req, res, next) {
     name: Joi.string().min(1),
     email: Joi.string().email(),
     phone: Joi.string().min(10),
+    subscription: Joi.string(),
+    password: Joi.string().min(8),
   });
 
   const validationResult = newContactValidation.validate(req.body);
 
   if (validationResult.error) {
-    res.status(400).send({ message: `Field ${validationResult.error.details[0].message}` });
+    res
+      .status(400)
+      .send({ message: `Field ${validationResult.error.details[0].message}` });
     return;
+  }
+
+  next();
+}
+
+function validateObjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).send();
   }
 
   next();
@@ -39,4 +58,5 @@ function validatePatchContact(req, res, next) {
 module.exports = {
   validateNewContact,
   validatePatchContact,
+  validateObjectId,
 };
